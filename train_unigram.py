3641b2ea3536fc3e0e41c5ab6a61bec13988f603
jojonki <junki.ohmura@gmail.com>
2019-01-05 20:40:17 +0900

    use argparse

--- train_unigram.py
+++ train_unigram.py
@@ -1,15 +1,21 @@
+import argparse
 from ngram import Ngram
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--train_file', type=str, metavar='PATH', help='train file path')
+parser.add_argument('--dump_file', type=str, metavar='PATH', help='tokenized result file')
+args = parser.parse_args()
 
 def test():
     ng = Ngram()
 
     # Your n-gram model is trained with a text file
-    ng.train('data/wiki-ja-train.word')
+    # ng.train('data/wiki-ja-train.word')
+    ng.train(args.train_file)
 
     # You can save your trained model as text. Currently, we do not support loading trained model.
-    ng.dump('trained/wiki_ja_train_trained_model', n=1)
-
+    # ng.dump('trained/wiki_ja_train_trained_model', n=1)
+    ng.dump(args.dump_file)
 
 
 if __name__ == '__main__':
