4d16334fd555107788023858ff6477f7a1b8d33e
jojonki <junki.ohmura@gmail.com>
2019-01-05 20:48:37 +0900

    rename file and use argparse

--- train_ngram.py
+++ train_ngram.py
@@ -2,6 +2,7 @@ import argparse
 from ngram import Ngram
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--N', type=int, default=1, help='N of n-gram')
 parser.add_argument('--train_file', type=str, metavar='PATH', help='train file path')
 parser.add_argument('--dump_file', type=str, metavar='PATH', help='tokenized result file')
 args = parser.parse_args()
@@ -15,7 +16,7 @@ def test():
 
     # You can save your trained model as text. Currently, we do not support loading trained model.
     # ng.dump('trained/wiki_ja_train_trained_model', n=1)
-    ng.dump(args.dump_file)
+    ng.dump('{}-{}gram'.format(args.dump_file, args.N), n=args.N)
 
 
 if __name__ == '__main__':
